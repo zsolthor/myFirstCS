c10b6df255a8c41897a1b6790ef8f7abb063bd24
zsolthor <horvath.zsolt82@gmail.com>
2021-02-12 15:19:48 +0100

    exercise 2 -3 ready 22-es leckéig kész (Section2ig)

---
+++ Exercise2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////Exercise2 - part 1 //////////
+            ///add meg órabért, amit check-olunk, hogy 7.5 alatt nem lehet és 49 felett sem.
+            
+            Console.WriteLine("Enter hourly pay rate:");
+            double rate = double.Parse(Console.ReadLine());
+            if (rate < 7.50 || rate > 49.00)
+            {
+                Console.WriteLine("Invalid rate");
+            }
+            else
+            {
+                Console.WriteLine("The rate is valid");
+            }
+
+
+            //////////Exercise2 - part 2 //////////
+            /// ad meg a hosszúságot és szélessgéet. 
+            /// 3 árkateógria van. 
+            /// 400 negyzetméter alatt: 25. 400 és 600 között: 35. és 600 felett már 50.
+            /// a megadott adatok alapján mond meg, hogy mennyi a heti díj és mennyi lesz a szezonra, ami 20 hét. 
+            /// 
+            Console.WriteLine("Enter the length of your yard");
+            int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width of your yard");
+            int width = int.Parse(Console.ReadLine());
+            int squareFeet = width * length;
+            int weeklyPrice;
+
+            if (squareFeet < 400)
+            {
+                weeklyPrice = 25;
+            }
+            else if (squareFeet >= 400 && squareFeet < 600)
+            {
+                weeklyPrice = 35;
+            }
+            else
+            {
+                weeklyPrice = 50;
+            }
+
+            Console.WriteLine("The weekly fee is {0}", weeklyPrice);
+            Console.WriteLine("The 20 week season price is: {0}", weeklyPrice * 20);
+
+            //////////Exercise2 - part 3 //////////
+            ///
+
+            Random randonGenerator = new Random();
+            int randomNumber = randonGenerator.Next(1,11);  //1 és 10 között véletlenszerű egész szám generálása
+            Console.WriteLine(randomNumber); // itt csak kiiratjuk, hogy tesztelni tudjuk 
+
+            Console.WriteLine("Guess a randm Number!");
+            int userGuess = int.Parse(Console.ReadLine());  //user-től kérünk egy egész számot
+
+            Console.WriteLine("The random number was {0}", randomNumber);  //megmondjuk mennyi volt a generált véletlen szám
+
+            if (userGuess > randomNumber)  //összehasonlítjuk, hogy nagyobb, kisebb vagy egyenlő volt a megadott és a generált szám.  
+            {
+                Console.WriteLine("Your guess was too high.");
+            }
+            else if (userGuess < randomNumber)
+            {
+                Console.WriteLine("Your guess was too low.");
+            }
+            else 
+            {
+                Console.WriteLine("Your guess was correct.");
+            }
+
+
+
+        }
+    }
+}
---
+++ fromLecture15/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fromLecture15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lecture 15: If Statements
+            //Lecture 16: If-Else Statements
+            //Lecture 17: Compound Expressions in If Statement
+            //Lecture 18: Switch statement
+            //Lecture 19: Conditional and Not Operators
+
+            //////////Lecture 15: If Statements//////////
+            int age = 5;
+            bool myBool15 = false;
+            if (myBool15)    //age < 10 vagy myBool15 simán if (myBool15)
+            { 
+            Console.WriteLine("Age is less than 10");
+            Console.WriteLine("Wow You are Young");
+            }
+
+            //////////Lecture 16: If-Else Statements//////////
+            Console.WriteLine("Enter Your Age:");
+            int age15_2 = int.Parse(Console.ReadLine());
+
+            if (age15_2 >= 18)
+            {
+                Console.WriteLine("You are good to go");
+            }
+            else
+            {
+                Console.WriteLine("You are to Young");
+            }
+
+
+            if (age15_2 == 1)
+            {
+                Console.WriteLine("You are 1");
+            }
+            else if (age15_2 == 2)
+            {
+                Console.WriteLine("You are 2");
+            }
+            else if (age15_2 == 3)
+            {
+                Console.WriteLine("You are 3");
+            }
+
+            //////////Lecture 17: Compound Expressions in If Statement//////////
+            
+            // && and operator
+            bool x17 = true;
+            bool y17 = true;
+            bool z17 = x17 && y17;
+
+            // || or operator 
+            bool z17_1 = x17 || y17;
+
+            Console.WriteLine("Enter Your Age:");
+            int age17 = int.Parse(Console.ReadLine());
+            if (age17 >= 18 && age17 <= 55)
+            {
+                Console.WriteLine("Good to go!");
+            }
+            else
+            {
+                Console.WriteLine("There is some problem. ");
+            }
+
+
+            Console.WriteLine("IMDB Enter Your Age:");
+            int age17_1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("IMDB What is the movie rating?");
+            Char rating = char.Parse(Console.ReadLine());
+
+            if (age17_1 <= 12 || age17_1 >= 65 && rating == 'G')  //((age17_1 <= 12 || age17_1 >= 65) && rating == 'G')
+            {
+                Console.WriteLine("Discount applies");
+            }
+            // 2   D   --> discount applies
+            // 15  G   --> ---
+            // 65   D   --> ---
+            // 65   G   --> discount applies
+
+
+            //////////Lecture 18: Switch statement//////////
+            Console.WriteLine("Enter Year:");
+            int year = int.Parse(Console.ReadLine());
+
+            switch (year)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Freshman");
+                        break;  //kell a break a c#-ban
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Sophomore");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Junior");
+                        break;
+                    }
+
+                case 4:
+                    {
+                        Console.WriteLine("Senior");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid Year");
+                        break;
+                    }               
+                    
+            }
+
+
+            //////////Lecture 19: Conditional and Not Operators//////////
+            // (condition) ? true : false
+            int x19 = 7;
+            int y19 = 6;
+
+            int biggestNumber = x19 > y19 ? x19 : y19;
+            //ha a kondició x nagyobb mint y, akkor kérdőjel utáni x lesz, ha hamis akkor y 
+            // (condition) ? true : false
+            Console.WriteLine(biggestNumber); //kiiratjuk az értéket
+
+            double testScore = 59.6;
+            Console.WriteLine((testScore >= 60)  ? "pass" : "fail");  //itt pass vagy fail lesz kiiratva
+
+            // Not operator 
+            // int myInt = 5;
+            // if (!(myInt >=6))
+            //{
+            //
+            //}
+
+        }
+    }
+}
