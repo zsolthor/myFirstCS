5a6a8bc2eba51681af751fe348fa6f5e91428da0
zsolthor <horvath.zsolt82@gmail.com>
2021-02-17 16:18:47 +0100

    Lecture 39 ready

---
+++ Exercise4/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Lecture 37 - Exercise4 - 1
+            // Lecture 38 - Exercise4 - 2
+            // Lecture 39 - Exercise4 - 3
+
+            ////////// Lecture 37 - Exercise4 - 1 //////////
+            ///szállítunk-e a megadott irányitószámra
+
+            bool found = false;
+
+            string[] zipCodes = { "10000", "10001", "10100", "10200", "103300", "10200", "15500", "80000", "80000", "90000", "18900", };
+            Console.WriteLine("Enter ZIP code:");
+            string code = Console.ReadLine();
+
+            for (int i = 0; i < zipCodes.Length; i++)
+            {
+                if (code == zipCodes[i])
+                {
+                    found = true;
+                }
+            }
+
+            if (found == true)
+            {
+                Console.WriteLine("We ship to this zip code");
+            }
+            else
+            {
+                Console.WriteLine("We does NOT ship to this zip code");
+            }
+
+            ////////// Lecture 38 - Exercise4 - 2 //////////
+            ///tanár hozzáadja neveket  és osztályzatokat, ő mondja meg mennyi diákot
+            ///diákok létre lesznek hozva és osztályzatok is. Az átlagot és a legjobb osztályzat meg kell jeleníteni, lkegjobb osztályzatot a névvel együtt.
+
+            Console.WriteLine("Enter the amount of students in Your class:");
+            int amountOfStudents = int.Parse(Console.ReadLine());  //megkérdezzük mennyi az osztály létszámunk
+
+            double averageGrade = 0;
+            double highestGrade = 0;
+            string highestGradeName = "";  // változok átlaghoz, legjobb osztéályzathoz és legjobb eredményt elért név
+
+            string[] studentNames = new string[amountOfStudents];
+            double[] grades = new double[amountOfStudents]; // Array létrehozva diákoknak és osztályzatoknak
+
+            for (int i2 = 0; i2 < amountOfStudents; i2++)
+            {
+                Console.WriteLine("Enter students #{0} name", i2 + 1);
+                string name = Console.ReadLine();
+                studentNames[i2] = name; //megkérdezzük student nevét +1 kell, mert 0 az első elem
+
+                Console.WriteLine("Enter student #{0} grade", i2 + 1);
+                double grade = double.Parse(Console.ReadLine());
+                grades[i2] = grade;  //név után egyből kérdezzük osztályzatot
+
+                averageGrade += grade;  // a for ciklusban osztályzatok össze lesznek adva
+
+                if (grade > highestGrade)  //ha osztályzat nagyobb mint eddig legnagyobb, akkor
+                {
+                    highestGrade = grade;  //a legnagyobb osztályzat lesz, amga az osztályzat
+                    highestGradeName = name; //elmentjük a student nevét  Itt még mondog for ciklusban vagyunk
+                }
+            }
+
+            //double average = averageGrade / amountOfStudents;    //ez is egy módszer
+            averageGrade /= amountOfStudents; //az összeget elmentettük már for ciklusban, msot elosztjuk a stdentek számával
+            Console.WriteLine("The average grade is: {0}", averageGrade);
+            Console.WriteLine("The highest grade is: {0} and their name was: {1}", highestGrade, highestGradeName);
+
+            ////////// Lecture 39 - Exercise4 - 3 //////////
+            ///
+
+            ////// kiegészítés feladathoz:
+            //string s = "Hello";
+            //Console.Write(s[0]); // hozzáferhetünk egy string első karakteréhez
+            //if (s.Contains("H")) // tartalmaz-e H karaktert a string
+            //{
+            //}
+            //s.Remove(0, 1); //melyik karaktertől, menniyt távolítson el
+            //s.Insert(0, "H"); //melyik helyre, mit helyezzen be
+            //Console.WriteLine("X darab karaktert tartalmaz: {0}", s.Length);
+
+            // get random word stuff
+            //string[] words = { "jump", "programming", "sky", "pictures", "river", "start", "grass", "google" };  //meghatározzuk a szókat, amit ki kell találni
+            string[] words = { "kiskutya", "narancs", "lekvár", "szivacs", "monitor", "lépegetés", "fürdés", "számitógép" }; // vagy ez a sor magyar vagy felső angol, csak bemutatás miatt
+            Random random = new Random();
+            int randomIndex = random.Next(0, 8); // generál egy véletlen számot 0...7 között, ez ugye index
+
+            string selectedWord = words[randomIndex]; //a words arrayből a random szám alapjáűn kiválaszt egy számot
+            string hiddenword = "";
+
+            for (int i3 = 0; i3 < selectedWord.Length; i3++)
+            {
+                hiddenword += "*";
+            }
+            Console.WriteLine(hiddenword);  // for ciklus 0-tól a véletlen kiválasztott szó karakterszámáig létrehoz egy ******** szót
+
+            // guessing stuff
+            while (hiddenword.Contains("*")) // a while addig fut, amíg van * karaterünk
+            {
+                Console.WriteLine("Word: {0}", hiddenword); //kiiratjuk a *****-os szót
+                Console.Write("Guess a letter >> "); // tippelni kell egy betűre
+                char letter = char.Parse(Console.ReadLine()); //amit karakterként tárolunk
+                bool containsLetter = false;
+
+                for (int i31 = 0; i31 < selectedWord.Length; i31++) //for ciklus végig megy minden betün 0-tól a kiválasztott szó karakterszám végéig
+                {
+                    if (selectedWord[i31] == letter)  //ha a karakter a szóban és a karakter, amit megadtunk tipp-nek megegyezik akkor ...
+                    {
+                        hiddenword = hiddenword.Remove(i31, 1);                  //... eltávolítunk egy csillagot i31 helyről, 1 karakter-t
+                        hiddenword = hiddenword.Insert(i31, letter.ToString());  // és i31-es helyre a karakter-t behelyezzük ,amit tippeltünk
+                        containsLetter = true;                                  // találatot rögzítjük egy bool változóba --> true lesz
+                    }
+                } //for ciklus vége itt
+
+                if (containsLetter == true) //ha volt találat
+                {
+                    Console.ForegroundColor = ConsoleColor.Green; // találat esetén zöld betű
+                    Console.WriteLine("Yes! {0} is in the word", letter); // kiratjuk, hogy volt találat és, hogy melyik betű
+                }
+                else //ha nem volt találat
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // ha nincs találat, akkor piros betű
+                    Console.WriteLine("Sorry! {0} is NOT in the word", letter); // kiratjuk, hogy melyik betű nem volt benne
+                }
+                Console.ResetColor(); // színeket resetlejük
+            }
+
+            //You won
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Congratulations! You win! The word was {0}", selectedWord); //ha nincs csillag karakter, akkor nyertél
+        }
+    }
+}
--- fromLecture31/Program.cs
+++ fromLecture31/Program.cs
@@ -1,36 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace fromLecture31
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Lecture 32: Creating an Array
-            // Lecture 33: Using Loops with Arrays 
+            // Lecture 33: Using Loops with Arrays
             // Lecture 34: Searching in Array Using Loops
-
+            // Lecture 35: Usefull Array Methods
+            // Lecture 36: Multidimensional arrays
 
             ////////// Lecture 32: Creating an Array //////////
 
-            int[] myArray = new int[3];     //element is an invidually item in the array. 
+            int[] myArray = new int[3];     //element is an invidually item in the array.
             myArray[0] = 5;                 //sub zero
             myArray[1] = 6;
             myArray[2] = 7;                 //ha 3 van definiláva ne legyen myArray[3] = 8;
 
             Console.WriteLine(myArray[0]);  // --> 5
 
-            int[] myArray2 = new int[4] {5,6,7,8};          //érték adás is van itt
-            int[] myArray3 = new int[] { 5, 6, 7, 8, 9};    //itt nincs szám, hogy mekkora array, mert érétkadásból magától adódik
-            int[] myArray4 = { 5, 6, 7};
+            int[] myArray2 = new int[4] { 5, 6, 7, 8 };          //érték adás is van itt
+            int[] myArray3 = new int[] { 5, 6, 7, 8, 9 };    //itt nincs szám, hogy mekkora array, mert érétkadásból magától adódik
+            int[] myArray4 = { 5, 6, 7 };
 
             ////////// Lecture 32: Using Loops with Arrays //////////
 
-            int[] grades = {66, 88, 95, 90, 100, 55, 99, 56};
+            int[] grades = { 66, 88, 95, 90, 100, 55, 99, 56 };
             //grades[0] += 3;
             //grades[1] += 3;
             //grades[2] += 3;
@@ -53,19 +50,17 @@ namespace fromLecture31
                 Console.WriteLine(grades2[i]);
             }
 
-            Console.WriteLine("Az array-ben {0} elem volt összesen",grades2.Length);
-
+            Console.WriteLine("Az array-ben {0} elem volt összesen", grades2.Length);
 
             //visszafelé olvasás
             int[] grades4 = { 10, 20, 30, 40, 50, 60, 65, 70, 75, 80 };
 
-            for (int i = grades4.Length -1; i >= 0; i--)
+            for (int i = grades4.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(grades4[i]);
             }
 
             Console.WriteLine("Az grades4 array-ben {0} elem volt összesen", grades4.Length);
-
 
             //foreach sample
             int[] grades3 = { 50, 60, 70, 80, 90, 100, 110, 120 };
@@ -74,7 +69,6 @@ namespace fromLecture31
             {
                 Console.WriteLine(score);
             }
-
 
             ////////// Lecture 34: Searching in Array Using Loops //////////
 
@@ -85,14 +79,14 @@ namespace fromLecture31
             string studentsName = Console.ReadLine();
 
             bool contains = false;
-            for (int i =0; i < students.Length; i++)
+            for (int i = 0; i < students.Length; i++)
             {
                 if (studentsName == students[i])
                 {
                     contains = true;
                     break;
-                }  //nem ide tettük az if es else-be a FOUND és NOT FOUND, mert akkor minden elemre mondta volna, hogy külön sorba, hogy megtalálta-e vagy sem. 
-                // annyira nem hatékokny, mert minden elemen végig megy. Ezért kell break. 
+                }  //nem ide tettük az if es else-be a FOUND és NOT FOUND, mert akkor minden elemre mondta volna, hogy külön sorba, hogy megtalálta-e vagy sem.
+                // annyira nem hatékokny, mert minden elemen végig megy. Ezért kell break.
             }
 
             if (contains == true)
@@ -108,7 +102,7 @@ namespace fromLecture31
             // !!!NEVER USED parallel array!!!
 
             string[] studentsP = { "Bob", "Ted", "Tom", "Fred" };
-            int[] gradesP = { 77, 50, 78, 99 };  
+            int[] gradesP = { 77, 50, 78, 99 };
 
             Console.WriteLine("Enter students name to search for:");
             string studentsNameP = Console.ReadLine();
@@ -123,8 +117,8 @@ namespace fromLecture31
                     contains = true;
                     studentGradeP = gradesP[i];
                     break;
-                }  //nem ide tettük az if es else-be a FOUND és NOT FOUND, mert akkor minden elemre mondta volna, hogy külön sorba, hogy megtalálta-e vagy sem. 
-                // annyira nem hatékokny, mert minden elemen végig megy. Ezért kell break. 
+                }  //nem ide tettük az if es else-be a FOUND és NOT FOUND, mert akkor minden elemre mondta volna, hogy külön sorba, hogy megtalálta-e vagy sem.
+                // annyira nem hatékokny, mert minden elemen végig megy. Ezért kell break.
             }
 
             if (contains == true)
@@ -137,9 +131,109 @@ namespace fromLecture31
                 Console.WriteLine("The student is NOT in the class");
             }
 
+            ////////// Lecture 35: Usefull Array Methods //////////
 
+            //binary search - közepétől indul, majd mondjuk jobbra középre, stb....
+            //!!!if information is ordered!!! more efficient that sequential
+            int[] myArray35 = { 10, 15, 20, 30, 35, 40, 45 };
+            Console.WriteLine("Enter number to check if it exists:");
+            int number35 = int.Parse(Console.ReadLine());       //sequential search method -- last lecture
 
+            int answer35 = Array.BinarySearch(myArray35, number35);     //BinarySearch-ben: (hol keresek, mit keresek)
 
+            if (answer35 < 0)
+            {
+                Console.WriteLine("The number does NOT exist");     //ha nincs akkor negatív szám a visszatérés
+            }
+            else
+            {
+                Console.WriteLine("The number exist, position is: {0}", answer35);      //ha van, akkor pozitív szám a visszatérés, hogy hol található !!!nullától indul!!!
+            }
+
+            //array sort method ----------------------------------------
+            int[] myArray35_1 = { 10, 5, 7, 2, 55 };
+            Array.Sort(myArray35_1);        //sorba rendezés
+
+            for (int i35_1 = 0; i35_1 < myArray35_1.Length; i35_1++)
+            {
+                Console.WriteLine(myArray35_1[i35_1]);
+            }
+
+            //reverse function ----------------------------------------
+            int[] myArray35_2 = { 10, 5, 7, 2, 55 };
+            Array.Reverse(myArray35_2);     //forditott sorrend
+
+            for (int i35_2 = 0; i35_2 < myArray35_2.Length; i35_2++)
+            {
+                Console.WriteLine(myArray35_2[i35_2]);
+            }
+
+            ////////// Lecture 36: Multidimensional arrays //////////
+            int[] myArray36 = { 5, 6, 7 };      // one dimension
+
+            //2 types: Rectangular array and Jagged array
+
+            //Rectangular array ----------------------------------------
+
+            int[,] myArray36_1 = new int[3, 4];      //[,] --> 2 dimensional array [3, 4] --> 3 rows, 4 columns
+            myArray36_1[0, 0] = 5;                      // ****
+            myArray36_1[0, 1] = 6;                      // ****
+            myArray36_1[0, 2] = 7;                      // ****
+            myArray36_1[0, 3] = 8;  //X and Y coordinates
+
+            myArray36_1[1, 0] = 6;
+            myArray36_1[1, 1] = 7;
+            myArray36_1[1, 2] = 8;
+            myArray36_1[1, 3] = 9;
+
+            myArray36_1[2, 0] = 7;
+            myArray36_1[2, 1] = 8;      // 5 6 7 8
+            myArray36_1[2, 2] = 9;      // 6 7 8 9
+            myArray36_1[2, 3] = 10;     // 7 8 9 10
+
+            for (int i36_1 = 0; i36_1 < 3; i36_1++)     //nested loop
+            {
+                for (int j36_1 = 0; j36_1 < 4; j36_1++)
+                {
+                    Console.Write(myArray36_1[i36_1, j36_1]);
+                }
+                Console.WriteLine(); //első sor sortörés
+            }
+
+            int[,] myArray36_2 =
+            { { 5,6,7,8 },
+              { 5,6,7,8 },
+              { 5,6,7,8 }
+            };
+
+            for (int i36_2 = 0; i36_2 < 3; i36_2++)     //nested loop
+            {
+                for (int j36_2 = 0; j36_2 < 4; j36_2++)
+                {
+                    Console.Write("{0,-2}", myArray36_2[i36_2, j36_2]);  //"{0,-2}", myArray36_2[i36_2, j36_2] space a számok közé
+                }
+                Console.WriteLine(); //első sor sortörés
+            }
+
+            //Jagged array ----------------------------------------
+
+            int[][] myArray36_3 = new int[3][];  //3 sorom van, de minden sor egy külön array valahány elemmel
+            //myArray36_3[0] = new int[4];
+            //myArray36_3[1] = new int[10];
+            //myArray36_3[2] = new int[2];
+            myArray36_3[0] = new int[4] { 5, 6, 7, 8 };
+            myArray36_3[1] = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            myArray36_3[2] = new int[2] { 1, 2 };  //array-ek definiálása
+
+            for (int i36_3 = 0; i36_3 < 3; i36_3++)     //nested loop  //sorokért felelős itt
+            {
+                for (int j36_3 = 0; j36_3 < myArray36_3[i36_3].Length; j36_3++)
+                {
+                    Console.Write("{0,-2}", myArray36_3[i36_3][j36_3]);  //"{0,-2}", myArray36_2[i36_2, j36_2] space a számok közé
+                                                                         // !!! myArray után itt [] [] lesz !!!
+                }
+                Console.WriteLine(); //első sor sortörés
+            }
         }
     }
 }
