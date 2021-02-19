4e26f9b0d5c3e41996e2c67605545dde51557cb3
zsolthor <horvath.zsolt82@gmail.com>
2021-02-19 17:00:43 +0100

    Lecture 55 ready

--- fromLecture51/Program.cs
+++ fromLecture51/Program.cs
@@ -6,49 +6,76 @@ using System.Threading.Tasks;
 
 namespace fromLecture51
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Lecture 52: Creating a Class
             // Lecture 53: Constructors
+            // Lecture 54: Get and Set Methods
+            // Lecture 55: Properties
+
+
 
             ////////// Lecture 52: Creating a Class //////////
-            Student s1 = new Student("Bob", 15, 65);  //V1   //create a student object; one version of the student blueprint
+            Student s1 = new Student("Bob", "Bobby",18, 65);  //V1   //create a student object; one version of the student blueprint
+            
             //s1.name = "Bob";  //s1. után látszanak: Name, Age, FinalGrade
             //s1.age =15;
             //s1.finalGrade = 65; //Student 1
-            s1.SayHello();  //s1. után SayHello megjelenik, ami a student class-ban van. 
+            s1.SayHello();  //s1. után SayHello megjelenik, ami a student class-ban van.
 
-            // a constructor nélküliben ha kihagy név, nem jelez.  
-            //I can't force the user to enter in a name, because it's just a variable and anyone has access ti it.           
+            // a constructor nélküliben ha kihagy név, nem jelez.
+            //I can't force the user to enter in a name, because it's just a variable and anyone has access ti it.
 
-            Student s2 = new Student("Frank", 12,90);  //V1  //ha kihagyod nevet, akkor constructor miatt error lesz
-            //s2.name = "Frank";  
+            Student s2 = new Student("Frank", "Franky", 12, 90);  //V1  //ha kihagyod nevet, akkor constructor miatt error lesz
+            //s2.name = "Frank";
             //s2.age = 12;
             //s2.finalGrade = 90; //Student 2
             s2.SayHello();
 
-            Student s3 = new Student("Ted",15,95);  //V1
+            Student s3 = new Student("Ted", "Teddy", 15, 95);  //V1
             //s3.name = "Ted";
             //s3.age = 15;
             //s3.finalGrade = 95; //Student 3
             s3.SayHello();
 
-            Student s4 = new Student("Zsolt", 32);  //V2
+            Student s4 = new Student("Zsolt", "Zsolti", 32);  //V2
             s4.SayHello();
 
             //Console.WriteLine(s1.name);  // private után nem elérhető!!!
 
             ////////// Lecture 53: Constructors ////////
             // a constructor is a special type of method (method, function, some type of action)
-            // the constructor is basically the parameters for the entire class. 
+            // the constructor is basically the parameters for the entire class.
             // Same thing works with when I build a function, the parameters are the required information that you need to supply the function in order to use it.
 
 
+            ////////// Lecture 54: Get and Set Methods ////////// 
+            //Console.WriteLine(s1.GetName());  //GetName megy, ha 18 vagy elmúlt 18
+
+            //s1.SetName(""); //SetName-ben be lett állítva, hogy üres nem lehet 
+            //Console.WriteLine(s1.GetName());
+            /// Lecture 55 első 2 sorát ki kell kommentelni A: A vagy B lehet
+
+            s1.SetAge(0); //SetAge-ben be lett állítva, hogy nulla nem lehet.
+            Console.WriteLine(s1.GetAge());
+
+            s1.SetFinalGrade(40); //65-re állítja
+            Console.WriteLine(s1.GetFinalGrade());
+            s1.SetFinalGrade(109); //100-ra állítja
+            Console.WriteLine(s1.GetFinalGrade());
+
+            ////////// Lecture 55: Properties //////////
+            ///
+
+            s1.Name = "";  //s1.SetName(""); helyett s1.Name = "";
+            Console.WriteLine(s1.Name);  //s1. után Name felajánl 
+            /// Lecture 54 első 3 sorát ki kell kommentelni B: A vagy B lehet
+
+            s4.lastName = "ZSoltika";
+            Console.WriteLine(s4.lastName);
+
         }
-
-
-
     }
 }
--- fromLecture51/Student.cs
+++ fromLecture51/Student.cs
@@ -1,18 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace fromLecture51
 {
-    class Student
+    internal class Student
     {
         ////////// Lecture 52: Creating a Class //////////
         private string name;     //ha ez private, akkor program nem látja pl: s1.Name = "Bob"; de SayHello a programban okés, mert az itt lett az osztályban megírva.
         private int age;
         private double finalGrade;
 
+        // Lecture 55: properties --auto-implemented property
+        public string lastName { get; set; }  //-olvasni és írni is lehet 
+        //public string lastName { private get; set }  -- olvasni nem lehet, írni igen 
+        //public string lastName { get; private set }  -- írni nem lehet, olvasni igen 
+        // ki kellett több helyen egészíteni lastName-mel 
+        //lecture 55 vége
 
 
         public void SayHello()
@@ -21,28 +23,158 @@ namespace fromLecture51
         }
 
         ////////// Lecture 53: Constructors ////////
-
         // a constructor is a special type of method (method, function, some type of action)
-        // #1: constructor has no return type. 
-        // #2. the name of the special function is always the name of the class. 
-        // #3. must be public: has to be seen by things outside of the class. 
+        // #1: constructor has no return type.
+        // #2. the name of the special function is always the name of the class.
+        // #3. must be public: has to be seen by things outside of the class.
 
-        public Student(string name, int age, double finalGrade)  //V1
+        public Student(string name, string lastName, int age, double finalGrade)  //V1
         {
             this.name = name;
             this.age = age;
             this.finalGrade = finalGrade;
         }
 
-        //overloading a constructor       
-        public Student(string name, int age) :this (name, age,0) //V2  //felső rész 3-ról 4-re nő a references
+        //overloading a constructor
+        public Student(string name, string lastName, int age) : this(name, lastName, age, 0) //V2  //felső rész 3-ról 4-re nő a references
         {                                       //constructor initializer:allows us to call another constructor from a constructor
-                                                //finalGrade itt default value to zero. 
-            //this.name = name;
-            //this.age = age;
-            //!!!itt van egy kis kód ismnétlés
+                                                //finalGrade itt default value to zero.
+                                                //this.name = name;
+                                                //this.age = age;
+                                                //!!!itt van egy kis kód ismnétlés
         }
 
+
+        ////////// Lecture 54: Get and Set Methods ////////// 
+        ///name, age, finalGrade be lett állítva private-be
+        ///get - ki lehet olvasni 
+        ///set lehet változtatni
+        ///ezek nélkül nem lehetett, mert private
+
+        //public string GetName()
+        //{
+        //    if (age >= 18)
+        //    {
+        //        return name;
+        //    }
+        //    else
+        //    {
+        //        return "this student is too young.";
+        //    }
+        //}
+
+        //public void SetName(string newName)  // nincs visszatérési értéke, ezért void
+        //{
+        //    if (newName != "")
+        //    {
+        //    this.name = newName;
+        //    }
+        //} // Lecture 55 public string Name ki kell kommentelni, ha ez aktív
+
+        public int GetAge()
+        {
+
+            if (age > 18)
+            {
+                return age;
+            }
+            else
+            {
+                return -1;
+            }
+        } // ki lehet olvasni nevet, ha elmúlt 18 éves
+
+        public void SetAge(int newAge)
+        {
+            if (newAge != 0)
+            { 
+            age = newAge;  //this.age = newAge; ez is lehetne, szerintem ez szebb
+            }
+        } // be lehet állítani, hogy menniy idős, ha nem nulla a megadott év
+
+        public double GetFinalGrade()
+        {
+            return finalGrade;
+        } // finalGrade ki lehet olvasni
+
+        public void SetFinalGrade(double newFinalGrade)
+        {
+            if (newFinalGrade < 65)
+            {
+                newFinalGrade = 65;
+            }
+            else if (newFinalGrade > 100)
+            {
+                newFinalGrade = 100;
+            }
+            finalGrade = newFinalGrade;
+        } // meg lehet változtatni finalGrade-t, de ha 65-nél kevesebb, akkor 65-re állítja, ha 100-nál nagyobb akkor 100-ra állítja
+
+        ////////// Lecture 55: Properties //////////
+        /// csak c#, előző máshol is
+
+        public string Name
+        {
+            get
+            {
+                if (age >= 18)
+                {
+                    return name;
+                }
+                else
+                {
+                    return "this student is too young.";
+                }
+            }
+            set
+            {
+                if (value != "")  // ez volt másolva előző leckéből: newName != ""
+                {
+                    this.name = value; // ez volt másolva előző leckéből: this.name = newName;
+                } // value kell!!!
+            }
+        } // Lecture 54 GetName, SetName ki kell kommentelni ha ez aktív
+
+        public int Age
+        {
+            get
+            {
+                if (age > 18)
+                {
+                    return age;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            set 
+            {
+                if (value != 0)
+                {
+                    age = value;  //this.age = newAge; ez is lehetne, szerintem ez szebb
+                }
+            }
+        } //megírtam, de nincs használva
+        public double FinalGrade
+        {
+            get
+            {
+                return finalGrade;
+            }
+            set
+            {
+                if (value < 65)
+                {
+                    value = 65;
+                }
+                else if (value > 100)
+                {
+                    value = 100;
+                }
+                finalGrade = value;
+            }
+        } //megírtam, de nincs használva
 
 
     }
